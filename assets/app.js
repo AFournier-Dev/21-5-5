2f5b0e0ebd366615235a81afed4c5f3ce7768cb7
AFournier-Dev <alexandre.fournier@yahoo.fr>
2021-05-03 22:35:37 +0200

    TBD clean css / Require Boostrap

---
+++ assets/app.js
@@ -0,0 +1,47 @@
+/* BURGER  */
+console.log("test");
+const spanMenu = document.getElementById("spanmenu");
+const menuToggle = document.getElementById("menuToggle");
+console.log(spanMenu);
+spanMenu.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log("j'ai cliqué sur le burger");
+    menuToggle.classList.toggle("open");
+});
+
+
+
+
+
+
+
+
+
+/*  Leaflet */
+
+var mymap = L.map('mapid', {
+    scrollWheelZoom: false,
+    zoomControl: false
+}).setView([51.505, -0.09], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWZvdXJuaWVyLWRldiIsImEiOiJja2ZwMnhsNDMwaGxkMnFsaGRvaTYxNHcxIn0.DVj-JmaNuORh4lhWwiE77Q', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+}).addTo(mymap);
+
+
+
+
+
+
+
+/*  carte de voeux */
+const lines = document.querySelectorAll('line')
+console.log(lines)
+setTimeout(() => {
+    lines.forEach(line => {
+        line.classList.add('Transmition');
+    });
+},  5000);
